a703ea75422dc85245074f07279203df61431867
Laura <laura.gonzalez@guardian.co.uk>
2018-04-26 12:46:39 +0100

    add BT, AOL

--- app/com/gu/identity/frontend/models/EmailProvider.scala
+++ app/com/gu/identity/frontend/models/EmailProvider.scala
@@ -30,6 +30,20 @@ case object YahooEmailProvider extends EmailProvider {
   val inboxLink = "https://mail.yahoo.com/"
 }
 
+case object BtEmailProvider extends EmailProvider {
+  val id = "bt"
+  val name = "BT Mail"
+  val matches = Seq("@btinternet.")
+  val inboxLink = "https://btmail.bt.com/cp/ps/main/index#mail"
+}
+
+case object AolEmailProvider extends EmailProvider {
+  val id = "bt"
+  val name = "BT Mail"
+  val matches = Seq("@aol.")
+  val inboxLink = "https://mail.aol.com/"
+}
+
 case object OutlookEmailProvider extends EmailProvider {
   val id = "outlook"
   val name = "Outlook.com"
@@ -38,7 +52,7 @@ case object OutlookEmailProvider extends EmailProvider {
 }
 
 object EmailProvider {
-  def all: Seq[EmailProvider] = Seq(GmailEmailProvider, YahooEmailProvider, OutlookEmailProvider)
+  def all: Seq[EmailProvider] = Seq(GmailEmailProvider, YahooEmailProvider, OutlookEmailProvider, BtEmailProvider, AolEmailProvider)
 
   def apply(emailProvider: Option[String]): Option[EmailProvider] =
     all.find(_.id == emailProvider.getOrElse(""))
