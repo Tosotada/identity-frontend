80d88df85e3a436ccbd900161a6b738d23266bd1
Laura <laura.gonzalez@guardian.co.uk>
2018-06-12 14:35:30 +0100

    send clientId

--- public/components/smartlock-trigger/smartlock-trigger.js
+++ public/components/smartlock-trigger/smartlock-trigger.js
@@ -2,6 +2,7 @@
 
 import qs from 'qs';
 import { route } from 'js/config';
+import { fetchTracker } from 'components/analytics/ga';
 
 const selector: string = '.smartlock-trigger';
 
@@ -21,22 +22,27 @@ const smartLockSignIn = (
   returnUrl: string,
   csrfToken: string
 ) => {
-  fetch(route('smartlockSignIn'), {
-    credentials: 'same-origin',
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: qs.stringify({
-      email: credentials.id,
-      password: credentials.password,
-      csrfToken
-    })
-  }).then(r => {
-    if (r.status === 200) {
-      window.location.href = returnUrl;
-    } else {
-      throw new Error(ERR_FAILED_SIGNIN);
-    }
-  });
+  new Promise(fetchTracker)
+    .then(tracker =>
+      fetch(route('smartlockSignIn'), {
+        credentials: 'same-origin',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: qs.stringify({
+          email: credentials.id,
+          password: credentials.password,
+          clientId: tracker.get('clientId'),
+          csrfToken
+        })
+      })
+    )
+    .then(r => {
+      if (r.status === 200) {
+        window.location.href = returnUrl;
+      } else {
+        throw new Error(ERR_FAILED_SIGNIN);
+      }
+    });
 };
 
 const init = ($element: HTMLElement): void => {
