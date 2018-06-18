bc693804ea5153b8d58abe2fca6116a5c20dea4a
Laura <laura.gonzalez@guardian.co.uk>
2018-06-18 13:51:34 +0100

    out with the old

--- public/main.js
+++ public/main.js
@@ -1,15 +1,7 @@
 import './components/sentry/sentry';
 
 import { loadComponents } from './js/load-components';
-
-import { isSupported as isBrowserSupported } from './components/browser/browser';
-
 import { logPageView } from './components/analytics/analytics';
-
-import { init as initSigninBindings } from './components/signin-form/signin-form';
 
 logPageView();
 loadComponents(document);
-if (isBrowserSupported) {
-  initSigninBindings();
-}
