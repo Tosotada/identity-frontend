d469590870a66d76a4796b8777da47a61ffc91a4
Laura <laura.gonzalez@guardian.co.uk>
2018-06-27 17:17:53 +0100

    async

--- public/components/react-island/react-island--collect-consents.js
+++ public/components/react-island/react-island--collect-consents.js
@@ -1,11 +1,13 @@
 // @flow
-import { CollectConsents } from 'elements/CollectConsents';
-import { hydrate } from 'js/hydrate-react-island';
-
 const selector: string = '.react-island--collect-consents';
 
 const init = ($component: HTMLElement): void => {
-  hydrate($component, CollectConsents);
+  Promise.all([
+    import('elements/CollectConsents'),
+    import('js/hydrate-react-island')
+  ]).then(([{CollectConsents}, {hydrate}]) => {
+    hydrate($component, CollectConsents);
+  });
 };
 
 export { selector, init };
