ba77cad6ea17427f73696e1b38cda7c3912af841
Laura <laura.gonzalez@guardian.co.uk>
2018-07-17 13:30:21 +0100

    make raven async

--- public/components/sentry/sentry.js
+++ public/components/sentry/sentry.js
@@ -1,17 +1,84 @@
-/* eslint-disable */
-/**
- * Use sentry to record Javascript errors
- */
+import { get as getConfig } from 'js/config';
 
-/*global console*/
+const onRavenLoaded = new Promise((done, err) => {
+  /* eslint-disable */
+  (function(_window, _document, _script, _onerror, _onunhandledrejection) {
+    const SENTRY_SDK = _window.SENTRY_SDK;
 
-import Raven from 'raven-js';
+    // Create a namespace and attach function that will store captured exception
+    // Because functions are also objects, we can attach the queue itself straight to it and save some bytes
+    var queue = function(exception) {
+      queue.data.push(exception);
+    };
+    queue.data = [];
 
-import { configuration } from '../configuration/configuration';
+    // Store reference to the old `onerror` handler and override it with our own function
+    // that will just push exceptions to the queue and call through old handler if we found one
+    const _oldOnerror = _window[_onerror];
+    _window[_onerror] = function(message, source, lineno, colno, exception) {
+      // Use keys as "data type" to save some characters"
+      queue({
+        e: [].slice.call(arguments)
+      });
 
-function init() {
-  if (configuration.sentryDsn) Raven.config(configuration.sentryDsn).install();
+      if (_oldOnerror) _oldOnerror.apply(_window, arguments);
+    };
+
+    // Do the same store/queue/call operations for `onunhandledrejection` event
+    const _oldOnunhandledrejection = _window[_onunhandledrejection];
+    _window[_onunhandledrejection] = function(exception) {
+      queue({
+        p: exception.reason
+      });
+      if (_oldOnunhandledrejection)
+        _oldOnunhandledrejection.apply(_window, arguments);
+    };
+
+    // Once our SDK is loaded
+    import('raven-js').then(Raven => {
+      try {
+        // Restore onerror/onunhandledrejection handlers
+        _window[_onerror] = _oldOnerror;
+        _window[_onunhandledrejection] = _oldOnunhandledrejection;
+
+        const data = queue.data;
+        const SDK = Raven;
+        // Configure it using provided DSN and config object
+        init(Raven);
+        // Because we installed the SDK, at this point we have an access to TraceKit's handler,
+        // which can take care of browser differences (eg. missing exception argument in onerror)
+        const tracekitErrorHandler = _window[_onerror];
+
+        // And capture all previously caught exceptions
+        if (data.length) {
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].e) {
+              tracekitErrorHandler.apply(SDK.TraceKit, data[i].e);
+            } else if (data[i].p) {
+              SDK.captureException(data[i].p);
+            }
+          }
+        }
+        done(Raven);
+      } catch (o_O) {
+        err(o_O);
+      }
+    });
+  })(window, document, 'script', 'onerror', 'onunhandledrejection');
+});
+
+const init = Raven => {
+  const dsn = getConfig('sentryDsn');
+  if (dsn) Raven.config(dsn).install();
   else console.warn('Sentry configuration not found');
-}
+};
 
-init();
+const captureExceptionAsync = ex => {
+  onRavenLoaded.then(Raven => {
+    Raven.captureException(ex);
+  });
+};
+
+const getRaven = () => onRavenLoaded;
+
+export { getRaven, captureExceptionAsync };
--- public/js/hydrate-react-island.js
+++ public/js/hydrate-react-island.js
@@ -1,7 +1,7 @@
 // @flow
 import { createElement } from 'react';
 import { render } from 'react-dom';
-import Raven from 'raven-js';
+import { captureExceptionAsync } from 'components/sentry/sentry';
 
 const getBootstrap = ($component: HTMLElement): {} => {
   const $bootstrap: ?HTMLElement = $component.querySelector(
@@ -11,7 +11,7 @@ const getBootstrap = ($component: HTMLElement): {} => {
     try {
       return JSON.parse($bootstrap.innerText);
     } catch (err) {
-      Raven.captureException(err, JSON.stringify(err));
+      captureExceptionAsync(err);
       return {};
     }
   } else {
