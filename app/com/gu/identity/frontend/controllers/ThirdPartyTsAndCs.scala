e834bd9a82537641e790f751a1bcc1cb2c703553
Calum Campbell <campbell.cj@hotmail.co.uk>
2018-09-03 16:09:50 +0100

    redirect to /signin

--- app/com/gu/identity/frontend/controllers/ThirdPartyTsAndCs.scala
+++ app/com/gu/identity/frontend/controllers/ThirdPartyTsAndCs.scala
@@ -55,8 +55,7 @@ class ThirdPartyTsAndCs(
               case Right(result) => Future.successful(result)
               case Left(errors) if errors.exists(_.id == GetUserUnauthorizedErrorID) =>
                 logger.error(s"Unauthorised client login request: $errors")
-                val reauthenticateUrl = UrlBuilder("/reauthenticate", ReturnUrl(Some(request.uri), config), clientIdActual)
-                Future.successful(SeeOther(reauthenticateUrl))
+                Future.successful(SeeOther(UrlBuilder("/signin", ReturnUrl(Some(request.uri), config))))
               case Left(errors) =>
                 logger.error(s"Could not check user's group membership status, failed with error: $errors")
                 httpErrorHandler.onClientError(request, BAD_REQUEST, "Could not check user's group membership status")
