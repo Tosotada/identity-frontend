12dea044aee7942b1d8a7bd6a79becd281a42074
Laura <laura.gonzalez@guardian.co.uk>
2018-09-28 11:58:17 +0100

    Add visible email field

--- app/com/gu/identity/frontend/models/text/TwoStepSignInChoicesPageText.scala
+++ app/com/gu/identity/frontend/models/text/TwoStepSignInChoicesPageText.scala
@@ -24,6 +24,7 @@ object TwoStepSignInChoicesPageText {
       "privacyPolicy" -> messages("signin.privacypolicy"),
 
       "emailFieldTitle" -> messages("signinTwoStep.emailFieldTitle"),
+      "emailTitle" -> messages("signinTwoStep.emailTitle"),
       "setPasswordTitle" -> messages("signinTwoStep.setPasswordTitle"),
       "setPasswordAction" -> messages("signinTwoStep.setPasswordAction"),
       "recoverPasswordAction" -> messages("signinTwoStep.recoverPasswordAction"),
